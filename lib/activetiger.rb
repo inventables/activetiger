77d24a6bae2b321716e643861f8632f7be92e517
Matt Gornick <mattgornick@gmail.com>
2011-03-15 17:08:41 -0500

    Requiring YAML on gem load.  Specs pass

--- lib/activetiger.rb
+++ lib/activetiger.rb
@@ -1,8 +1,8 @@
 $:.unshift File.dirname(__FILE__)
 
 require 'rubygems'
+require 'yaml'
 require 'restclient'
 require 'activetiger/configuration'
 require 'activetiger/gateway'
 require 'activetiger/response'
-
